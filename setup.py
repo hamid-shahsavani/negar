73df3aa0ca2062b02d84fe5fbe1b53b143749a1b
sys113 <24222077+sys113@users.noreply.github.com>
2019-11-29 06:26:00 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup , find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+  name = "negar",
+  version = "0.1.0",
+  description = "call negar in your python source code and log to a file ...",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author = "SYS113",
+  author_email = "051.SYS113@gmail.com",
+  url = "https://github.com/sys113/negar",
+  keywords = ["negar", "log", "save"],
+  packages = find_packages(),
+  install_requires=['tzlocal'],
+  classifiers=[
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: Implementation",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+  ],
+)
