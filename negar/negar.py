a6ed1d9a17869d113e38df614f629e27b41e841d
hameda169 <169hamed@gmail.com>
2019-12-04 22:59:26 +0330

    countries with their capitals are moved to countriesWithTheirCapital.json

--- negar/negar.py
+++ negar/negar.py
@@ -8,6 +8,7 @@ from platform import system, release, machine
 from getpass import getuser
 from os.path import isfile
 from inspect import getframeinfo, stack
+from json import load
 
 
 # create log function ...
@@ -149,21 +150,20 @@ def log(text=None, save=None, size=None):
 
     continent = str(get_localzone()).lower().split('/')[0]
 
-    # get country ...
-
-    def get_country():
-        data = {'kabul': 'afghanistan', 'tirana': 'albania', 'alger': 'algeria', 'fagatogo': 'american samoa', 'andorra la vella': 'andorra', 'luanda': 'angola', 'the valley': 'anguilla', 'null': 'united states minor outlying islands', "saint john's": 'antigua and barbuda', 'buenos aires': 'argentina', 'yerevan': 'armenia', 'oranjestad': 'aruba', 'canberra': 'australia', 'wien': 'austria', 'baku': 'azerbaijan', 'nassau': 'bahamas', 'al-manama': 'bahrain', 'dhaka': 'bangladesh', 'bridgetown': 'barbados', 'minsk': 'belarus', 'bruxelles [brussel]': 'belgium', 'belmopan': 'belize', 'porto-novo': 'benin', 'hamilton': 'bermuda', 'thimphu': 'bhutan', 'la paz': 'bolivia', 'sarajevo': 'bosnia and herzegovina', 'gaborone': 'botswana', 'brasília': 'brazil', 'bandar seri begawan': 'brunei', 'sofia': 'bulgaria', 'ouagadougou': 'burkina faso', 'bujumbura': 'burundi', 'phnom penh': 'cambodia', 'yaound': 'cameroon', 'ottawa': 'canada', 'praia': 'cape verde', 'george town': 'cayman islands', 'bangui': 'central african republic', "n'djam": 'chad', 'santiago de chile': 'chile', 'peking': 'china', 'flying fish cove': 'christmas island', 'west island': 'cocos (keeling) islands', 'santaf': 'colombia', 'moroni': 'comoros', 'brazzaville': 'congo', 'avarua': 'cook islands', 'san jos': 'costa rica', 'zagreb': 'croatia', 'la habana': 'cuba', 'nicosia': 'cyprus', 'praha': 'czech republic', 'copenhagen': 'denmark', 'djibouti': 'djibouti', 'roseau': 'dominica', 'santo domingo de guzm': 'dominican republic', 'dili': 'east timor', 'quito': 'ecuador', 'cairo': 'egypt', 'san salvador': 'el salvador', 'london': 'united kingdom', 'malabo': 'equatorial guinea', 'asmara': 'eritrea', 'tallinn': 'estonia', 'addis abeba': 'ethiopia', 'stanley': 'falkland islands', 'tórshavn': 'faroe islands', 'suva': 'fiji islands', 'helsinki [helsingfors]': 'finland', 'paris': 'france', 'cayenne': 'french guiana', 'papeete': 'french polynesia', 'libreville': 'gabon', 'banjul': 'gambia', 'tbilisi': 'georgia', 'berlin': 'germany', 'accra': 'ghana', 'gibraltar': 'gibraltar', 'athenai': 'greece', 'nuuk': 'greenland', "saint george's": 'grenada', 'basse-terre': 'guadeloupe', 'aga': 'guam', 'ciudad de guatemala': 'guatemala', 'conakry': 'guinea', 'bissau': 'guinea-bissau', 'georgetown': 'guyana', 'port-au-prince': 'haiti', 'citt': 'holy see (vatican capital state)', 'tegucigalpa': 'honduras', 'victoria': 'seychelles', 'budapest': 'hungary', 'reykjav': 'iceland', 'new delhi': 'india', 'jakarta': 'indonesia', 'tehran': 'iran', 'baghdad': 'iraq', 'dublin': 'ireland', 'jerusalem': 'israel', 'roma': 'italy', 'yamoussoukro': 'ivory coast', 'kingston': 'norfolk island', 'tokyo': 'japan', 'amman': 'jordan', 'astana': 'kazakhstan', 'nairobi': 'kenya', 'bairiki': 'kiribati', 'kuwait': 'kuwait', 'bishkek': 'kyrgyzstan', 'vientiane': 'laos', 'riga': 'latvia', 'beirut': 'lebanon', 'maseru': 'lesotho', 'monrovia': 'liberia', 'tripoli': 'libyan arab jamahiriya', 'vaduz': 'liechtenstein', 'vilnius': 'lithuania',
-                'luxembourg [luxemburg/l': 'luxembourg', 'macao': 'macao', 'skopje': 'north macedonia', 'antananarivo': 'madagascar', 'lilongwe': 'malawi', 'kuala lumpur': 'malaysia', 'male': 'maldives', 'bamako': 'mali', 'valletta': 'malta', 'dalap-uliga-darrit': 'marshall islands', 'fort-de-france': 'martinique', 'nouakchott': 'mauritania', 'port-louis': 'mauritius', 'mamoutzou': 'mayotte', 'ciudad de m': 'mexico', 'palikir': 'micronesia, federated states of', 'chisinau': 'moldova', 'monaco-ville': 'monaco', 'ulan bator': 'mongolia', 'plymouth': 'montserrat', 'rabat': 'morocco', 'maputo': 'mozambique', 'rangoon (yangon)': 'myanmar', 'windhoek': 'namibia', 'yaren': 'nauru', 'kathmandu': 'nepal', 'amsterdam': 'netherlands', 'willemstad': 'netherlands antilles', 'noum': 'new caledonia', 'wellington': 'new zealand', 'managua': 'nicaragua', 'niamey': 'niger', 'abuja': 'nigeria', 'alofi': 'niue', 'pyongyang': 'north korea', 'belfast': 'northern ireland', 'garapan': 'northern mariana islands', 'oslo': 'norway', 'masqat': 'oman', 'islamabad': 'pakistan', 'koror': 'palau', 'gaza': 'palestine', 'ciudad de panam': 'panama', 'port moresby': 'papua new guinea', 'asunci': 'paraguay', 'lima': 'peru', 'manila': 'philippines', 'adamstown': 'pitcairn', 'warszawa': 'poland', 'lisboa': 'portugal', 'san juan': 'puerto rico', 'doha': 'qatar', 'saint-denis': 'reunion', 'bucuresti': 'romania', 'moscow': 'russian federation', 'kigali': 'rwanda', 'jamestown': 'saint helena', 'basseterre': 'saint kitts and nevis', 'castries': 'saint lucia', 'saint-pierre': 'saint pierre and miquelon', 'kingstown': 'saint vincent and the grenadines', 'apia': 'samoa', 'san marino': 'san marino', 's': 'sao tome and principe', 'riyadh': 'saudi arabia', 'edinburgh': 'scotland', 'dakar': 'senegal', 'freetown': 'sierra leone', 'singapore': 'singapore', 'bratislava': 'slovakia', 'ljubljana': 'slovenia', 'honiara': 'solomon islands', 'mogadishu': 'somalia', 'pretoria': 'south africa', 'seoul': 'south korea', 'juba': 'south sudan', 'madrid': 'spain', 'khartum': 'sudan', 'paramaribo': 'suriname', 'longyearbyen': 'svalbard and jan mayen', 'mbabane': 'swaziland', 'stockholm': 'sweden', 'bern': 'switzerland', 'damascus': 'syria', 'dushanbe': 'tajikistan', 'dodoma': 'tanzania', 'bangkok': 'thailand', 'lom': 'togo', 'fakaofo': 'tokelau', "nuku'alofa": 'tonga', 'port-of-spain': 'trinidad and tobago', 'tunis': 'tunisia', 'ankara': 'turkey', 'ashgabat': 'turkmenistan', 'cockburn town': 'turks and caicos islands', 'funafuti': 'tuvalu', 'kampala': 'uganda', 'kyiv': 'ukraine', 'abu dhabi': 'united arab emirates', 'washington': 'united states', 'montevideo': 'uruguay', 'toskent': 'uzbekistan', 'port-vila': 'vanuatu', 'caracas': 'venezuela', 'hanoi': 'vietnam', 'road town': 'virgin islands, british', 'charlotte amalie': 'virgin islands, u.s.', 'cardiff': 'wales', 'mata-utu': 'wallis and futuna', 'el-aai': 'western sahara', 'sanaa': 'yemen', 'beograd': 'yugoslavia', 'lusaka': 'zambia', 'harare': 'zimbabwe'}
-        if city in data:
-            return data[city]
-        else:
-            return 'unknown'
-
-    country = str(get_country())
-
     # get city ...
 
     city = str(get_localzone()).lower().split('/')[1]
+
+    # get country ...
+
+    def get_country(_city):
+        data = load(open('negar/countriesWithTheirCapital.json', 'r'))
+        if _city in data:
+            return data[_city]
+        else:
+            return 'unknown'
+
+    country = str(get_country(city))
 
     # get username ...
 
@@ -222,8 +222,8 @@ def log(text=None, save=None, size=None):
         # write 'log' to log file ...
 
         log_file.write('  |   ' + str(1) + '   | ' + date + ' | ' + time + ' | ' + str(text) + ' ' * (
-                    len(specifications) - (len(log_file_text) + len(
-                log_file_python_file_name) + 45)) + '|' + log_file_python_file_name + '|' + log_file_python_file_line + '|' + '\n')
+                len(specifications) - (len(log_file_text) + len(
+            log_file_python_file_name) + 45)) + '|' + log_file_python_file_name + '|' + log_file_python_file_line + '|' + '\n')
 
         # write ' ___ ' to log file ...
 
@@ -274,9 +274,9 @@ def log(text=None, save=None, size=None):
         # add new 'log' to log file ...
 
         log_file.write('  |' + log_file_number + '| ' + date + ' | ' + time + ' | ' + str(text) + ' ' * (
-                    len(specifications) - (len(log_file_text) +
-                                           len(
-                                               log_file_python_file_name) + 45)) + '|' + log_file_python_file_name + '|' + log_file_python_file_line + '|' + '\n')
+                len(specifications) - (len(log_file_text) +
+                                       len(
+                                           log_file_python_file_name) + 45)) + '|' + log_file_python_file_name + '|' + log_file_python_file_line + '|' + '\n')
 
         # write ' ___ ' to log file ...
 
